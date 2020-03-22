d70723cffebdabd3adcbe1c91b7e9ed81f134f90
Stan Clarke, Jr <stan@stanclarkejr.ca>
2020-03-21 23:50:41 -0400

    Typos

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,48 +1,48 @@
 import React from "react"
 
 const buttonStyles = {
-    fontSize: `13px`,
-    textAlign: `center`,
-    color: `white`,
-    outline: `none`,
-    padding: `12px 60px`,
-    boxShadow: `2px 5px 10px rgba(0, 0, 0, .15)`,
-    backgroundColor: `rgba(81, 202, 197, .8)`,
-    borderRadius: `6px`,
-    letterSpacing: `1.5px`,
+  fontSize: `13px`,
+  textAlign: `center`,
+  color: `white`,
+  outline: `none`,
+  padding: `12px 60px`,
+  boxShadow: `2px 5px 10px rgba(0, 0, 0, .15)`,
+  backgroundColor: `rgba(81, 202, 197, .8)`,
+  borderRadius: `6px`,
+  letterSpacing: `1.5px`,
 }
 
 const Checkout = class extends React.Component {
-    // Initialize Stripe.js with your publishable key.
-    // You can find your key in the Dashboard:
-    // https://dashboard.stripe.com/account/apikeys
-    componentDidMount() {
-      this.stripe = window.Stripe(``)
-    }
+  // Initialize Stripe.js with your publishable key.
+  // You can find your key in the Dashboard:
+  // https://dashboard.stripe.com/account/apikeys
+  componentDidMount() {
+    this.stripe = window.Stripe(``)
+  }
 
-    async redirectToCheckout(event) {
-      event.preventDefault()
-      const { error } = await this.stripe.redirectToCheckout({
-        items: [{ sku: `sku_Goz0d7cnMgyTQI`, quantity: 1}],
-        successUrl: `${window.location.origin}/success/`,
-        cancelUrl: `${window.location.origin}/`,
-      })
+  async redirectToCheckout(event) {
+    event.preventDefault()
+    const { error } = await this.stripe.redirectToCheckout({
+      items: [{ sku: `sku_Goz0d7cnMgyTQI`, quantity: 1 }],
+      successUrl: `${window.location.origin}/success/`,
+      cancelUrl: `${window.location.origin}/`,
+    })
 
-      if (error) {
-        console.warn(`Error: `, error)
-      }
-    }
-
-    render() {
-      return (
-        <button
-          style={buttonStyles}
-          onClick={event => this.redirectToCheckout(event)}
-        >
-          BUY NOW
-        </button>
-      )
+    if (error) {
+      console.warn(`Error: `, error)
     }
   }
 
-  export default Checkout
+  render() {
+    return (
+      <button
+        style={buttonStyles}
+        onClick={event => this.redirectToCheckout(event)}
+      >
+        BUY NOW
+      </button>
+    )
+  }
+}
+
+export default Checkout
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `e-commerce`]} />
 
-    <Image/>
+    <Image />
 
     <div>
       <p>
-        1959 was a pivotal year in the history of jazz music. <em>Kind of Blue, Time Out, Mingus Ah Um and The Shape of Jazz to Come</em> &mdash; a handful of iconic jazz albums to be produced that year.
+        1959 was a pivotal year in the history of jazz music. <em>Kind of Blue, Time Out, Mingus Ah Um</em> and <em>The Shape of Jazz to Come</em> &mdash; a handful of iconic jazz albums to be produced that year.
       </p>
       <p>
         The stories behind these, and other albums that shaped jazz music.
